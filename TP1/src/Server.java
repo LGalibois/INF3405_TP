a538a5d2e61f02a97ce4421880453b4c9cd672e7
7K-G <60433364+Grem7@users.noreply.github.com>
2022-01-19 19:18:07 -0500

    MG: copied Server.java and ClientHandler.java

--- TP1/src/Server.java
+++ TP1/src/Server.java
@@ -1,4 +1,40 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-
+	private static ServerSocket listener;
+	
+	public static void main(String[] args) throws Exception
+	{
+		int clientNumber = 0;
+		
+		String serverAddress = "127.0.0.1";
+		int serverPort = 5000;
+		
+		listener = new ServerSocket();
+		listener.setReuseAddress(true);
+		InetAddress serverIp = InetAddress.getByName(serverAddress);
+		
+		listener.bind(new InetSocketAddress(serverIp, serverPort));
+		
+		System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
+		
+		try
+		{
+			while (true)
+			{
+				new ClientHandler(listener.accept(), clientNumber++).start();
+			}
+		}
+		finally
+		{
+			listener.close();
+		}
+	}
 }
+
+
