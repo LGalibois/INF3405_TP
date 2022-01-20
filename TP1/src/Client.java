ef255378cc1ae99595e41b44011b29c3d29215fb
LeonardGalibois <60432695+LeonardGalibois@users.noreply.github.com>
2022-01-19 20:17:46 -0500

    LG:ajout lecture info sur le client

--- TP1/src/Client.java
+++ TP1/src/Client.java
@@ -1,4 +1,6 @@
 import java.io.DataInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Client {
@@ -9,25 +11,34 @@ public class Client {
 	 */
 	public static void main(String[] args) throws Exception
 	{
-			// Adresse et port du serveur
-		String serverAddress = "127.0.0.1";
-		int port = 5000;
+
+		// Adresse et port du serveur	
+		BufferedReader screenReader = new BufferedReader(new InputStreamReader(System.in));
+		
+		String serverAddress = screenReader.readLine();
+		String port = screenReader.readLine();
+		
+		System.out.println(serverAddress + " " + port);
+		
+		String test = screenReader.readLine();
+		
+		String test2 = "";
 		
 		// Creation d'une nouvelle connexion avec le serveur
-		socket= new Socket(serverAddress, port);
+		//socket= new Socket(serverAddress, port);
 		
-		System.out.format("The server is running on %s :%d%n", serverAddress, port);
+		//System.out.format("The server is running on %s :%d%n", serverAddress, port);
 		
 		// Creation d'un canal entrant pour recevoir les message envoyes par le serveur
-		DataInputStream in = new DataInputStream(socket.getInputStream());
+		//DataInputStream in = new DataInputStream(socket.getInputStream());
 		
 		// Attente de la reception d'un message envoye par le serveur sur le canal
-		String helloMessageFromServer = in.readUTF();
-		System.out.println(helloMessageFromServer);
+		//String helloMessageFromServer = in.readUTF();
+		//System.out.println(helloMessageFromServer);
 		
 		//Fermeture de la connexion avec le serveur
-		
-		socket.close();
+
+		//socket.close();
 	}
 	
 	
