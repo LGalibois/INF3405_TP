caa4fc507860079c1202c31042491c59206439c3
Matis Grégoire <matis.gregoire@polymtl.ca>
2022-02-02 19:53:21 -0500

    MG: le serveur possède maintenant une liste de client qu'il passe à chaque ClientHandler. Le ClientHandler peut recevoir les messages du client et les envoyer aux autres clients

--- TP1/src/ClientHandler.java
+++ TP1/src/ClientHandler.java
@@ -1,26 +1,53 @@
 import java.io.DataOutputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.List;
 import java.net.Socket;
 
 public class ClientHandler extends Thread 
 {
+	int MAX_MESSAGE_LENGTH = 200;
+	
 	private Socket socket;
+	private List<ClientHandler> clients;
+	private DataOutputStream out;
+	private DataInputStream in;
 	private int clientNumber;
 	
-	public ClientHandler(Socket socket, int clientNumber)
+	public ClientHandler(Socket socket, List<ClientHandler> clients, int clientNumber)
 	{
 		this.socket = socket;
 		this.clientNumber = clientNumber;
+		this.clients = clients;
 		System.out.println("New connection with client#" + clientNumber + " at " + socket);
+	}
+	
+	private void onMessageReceived(String message) throws IOException {
+		for (ClientHandler client : clients) {
+			if (client != this) {
+				client.sendMessage(message);
+			}
+		}
+	}
+	
+	public void sendMessage(String message) throws IOException {
+		if (out != null) {
+			out.writeUTF(new String(message));
+		}
 	}
 	
 	public void run()
 	{
 		try
 		{
-			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			out = new DataOutputStream(socket.getOutputStream());
+			in = new DataInputStream(socket.getInputStream());
 			
-			out.writeUTF("Hello from server - you are client #" + clientNumber);
+			while (!socket.isClosed()) {
+				if (in.available() > 0) {
+					onMessageReceived(in.readUTF());
+				}
+			}
 		}
 		catch (IOException e)
 		{
