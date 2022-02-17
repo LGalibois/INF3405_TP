40f580f06929a3514cadf60802a8131282b5a58f
LeonardGalibois <60432695+LeonardGalibois@users.noreply.github.com>
2022-02-16 19:15:06 -0500

    LG: ajout de la fonction pour se connecter

--- TP1/src/Client.java
+++ TP1/src/Client.java
@@ -1,6 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.net.Socket;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
@@ -8,35 +9,53 @@ import java.time.LocalDateTime;
 import java.net.InetAddress;
 
 public class Client {
+	private final static String REGISTRATION_GRANTED_MESSAGE = "registration granted";
+	
 	private static Socket socket;
 	private static DataOutputStream out;
+	private static DataInputStream in;
+	private static Scanner consoleReader;
 	
 	public static void main(String[] args) throws Exception
 	{
-		Scanner consoleReader = new Scanner(System.in);	
+		consoleReader = new Scanner(System.in);	
 		
 		String serverAddress = InputValidator.getValidAddress(consoleReader);
 		int serverPort = InputValidator.getValidPort(consoleReader);
-		
-		System.out.println("Entrez votre username: ");
-		String username =  consoleReader.nextLine();
-		
-		System.out.println("Entrez votre mot de passe :");
-		String password = consoleReader.nextLine();
-		
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-		LocalDateTime now = LocalDateTime.now();
-		
-		InetAddress ip = InetAddress.getLocalHost();
 		
 		socket = new Socket(serverAddress, serverPort);
 		
 		// Creation d'un canal entrant pour recevoir les message envoyes par le serveur
 		out = new DataOutputStream(socket.getOutputStream());
-		out.writeUTF(username + " " + password);
+		in = new DataInputStream(socket.getInputStream());
 		
-		//Fermeture de la connexion avec le serveur
+		connect();
+		MessageReceiver messageReceiver = new MessageReceiver(socket);
+		messageReceiver.start();
 
-		//socket.close();
+		socket.close();
+	}
+	
+	public static void connect()
+	{
+		String response = "";
+		
+		do 
+		{
+			System.out.println("Entrez votre username: ");
+			String username = consoleReader.nextLine();
+			
+			System.out.println("Entrez votre mot de passe :");
+			String password = consoleReader.nextLine();
+			
+			try {
+				out.writeUTF(String.format("%s %s", username, password));
+				response = in.readUTF();
+			}
+			catch(Exception exception)
+			{
+				System.out.println(exception.getMessage());
+			}	
+		}while(response != REGISTRATION_GRANTED_MESSAGE);
 	}
 }
