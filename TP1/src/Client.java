1b1559b174c70534a0639301f707e6a7a9ea9c57
Ralphosido <ralphkh08@live.com>
2022-02-16 19:10:57 -0500

    Send message Function & exit

--- TP1/src/Client.java
+++ TP1/src/Client.java
@@ -2,6 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.net.Socket;
 import java.io.DataOutputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime; 
@@ -10,9 +11,12 @@ import java.net.InetAddress;
 public class Client {
 	private static Socket socket;
 	private static DataOutputStream out;
+	private static DataInputStream in; 
+
 	
 	public static void main(String[] args) throws Exception
 	{
+		
 		Scanner consoleReader = new Scanner(System.in);	
 		
 		String serverAddress = InputValidator.getValidAddress(consoleReader);
@@ -31,12 +35,44 @@ public class Client {
 		
 		socket = new Socket(serverAddress, serverPort);
 		
+	
 		// Creation d'un canal entrant pour recevoir les message envoyes par le serveur
 		out = new DataOutputStream(socket.getOutputStream());
-		out.writeUTF(username + " " + password);
 		
+	
+		out.writeUTF(username + " " + password);
+	
 		//Fermeture de la connexion avec le serveur
 
 		//socket.close();
+		String message;
+		do {
+		message = sendMessage();
+		}
+		while (message !=  "exit");
+		
+	}
+	public static String sendMessage()  {
+		
+		Scanner consoleReader = new Scanner(System.in);	
+		
+		String message;
+		do {
+		System.out.println("Entrez votre message");
+		message = consoleReader.nextLine();
+		}
+		while(!InputValidator.isMessageValid(message)); 
+		
+		try {
+			out.writeUTF(message);
+		}
+		
+				catch (Exception e) {
+					
+				}
+				
+		
+		return message;
+		
 	}
 }
